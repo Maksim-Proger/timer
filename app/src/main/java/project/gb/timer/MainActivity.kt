359298b302fe7e85c9df855ad8e650c8aa4f7471
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-03-09 23:14:56 +0300

    second commit

--- app/src/main/java/project/gb/timer/MainActivity.kt
+++ app/src/main/java/project/gb/timer/MainActivity.kt
@@ -1,20 +1,36 @@
 package project.gb.timer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import project.gb.timer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO надо разобраться как меняется цвет у SeekBar и ProgressBar
+        // Привязываем SeekBar к ProgressBar
+        binding.progressBar.max = 100
+        binding.seekBar.max = 100
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.progressBar.progress = progress
+                binding.textViewTimer.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO доработать
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO доработать
+            }
+        })
     }
 }
