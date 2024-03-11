a9bbd4b4d6a0c8d287c10a37beea1798db3c1215
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-03-12 01:17:05 +0300

    6-th commit

--- app/src/main/java/project/gb/timer/TimerFragment.kt
+++ app/src/main/java/project/gb/timer/TimerFragment.kt
@@ -1,100 +1,77 @@
 package project.gb.timer
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import project.gb.timer.databinding.FragmentTimerBinding
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-
 
 class TimerFragment : Fragment() {
 
-    private var _binding : FragmentTimerBinding? = null
+    private val viewModel: TimerViewModel by viewModels()
+    private var _binding: FragmentTimerBinding? = null
     private val binding get() = _binding!!
-    private var timerValue = 0
-    private val handler = Handler(Looper.getMainLooper())
-    private var timerRunnable: Runnable? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentTimerBinding.inflate(inflater)
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Привязываем SeekBar к ProgressBar
         binding.progressBar.max = 100
         binding.seekBar.max = 100
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 binding.progressBar.progress = progress
+                binding.indicatorTimer.progress = progress
                 binding.textViewTimer.text = progress.toString()
-                // Записываем значение
-                timerValue = progress
-
-                binding.indicatorTimer.max = progress
-            }
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-                // TODO доработать
-            }
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                // TODO доработать
+                viewModel.updateTimer(progress)
             }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
-        buttonsListener()
-    }
-
-    /**
-     * Запускаем таймер
-     */
-    private fun startTimer(){
-        timerRunnable  = object : Runnable {
-            override fun run() {
-                binding.textViewTimer.text = timerValue.toString()
-                val progressPercentage = (timerValue * 100 / binding.seekBar.max)
-                binding.indicatorTimer.progress = progressPercentage
-                timerValue--
-                handler.postDelayed(this, 1000)
-            }
-        }
-        handler.post(timerRunnable!!)
-    }
-
-    /**
-     * Останавливаем таймер
-     */
-    private fun stopTimer() {
-        timerRunnable?.let { handler.removeCallbacks(it) }
-        timerValue = 0
-        binding.textViewTimer.text = timerValue.toString()
-        binding.progressBar.progress = 0
-    }
-
-    private fun buttonsListener() {
-
         binding.buttonStart.setOnClickListener {
-            binding.buttonStop.isVisible = true
-            binding.buttonStart.isVisible = false
-            startTimer()
+            binding.buttonStop.visibility = View.VISIBLE
+            binding.buttonStart.visibility = View.INVISIBLE
+            viewModel.startTimer()
         }
 
         binding.buttonStop.setOnClickListener {
-            binding.buttonStart.isVisible = true
-            binding.buttonStop.isVisible = false
-            stopTimer()
+            binding.buttonStart.visibility = View.VISIBLE
+            binding.buttonStop.visibility = View.INVISIBLE
+            viewModel.stopTimer()
+        }
+
+        viewModel.timer.observe(viewLifecycleOwner, Observer { time ->
+            binding.textViewTimer.text = time.toString()
+            val progressPercentage = (time * 100 / binding.seekBar.max)
+            binding.progressBar.progress = progressPercentage
+            binding.indicatorTimer.progress = progressPercentage
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.timer.value?.let {
+            outState.putInt("timer_value", it)
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        savedInstanceState?.getInt("timer_value")?.let {
+            viewModel.updateTimer(it)
         }
     }
 
@@ -102,5 +79,6 @@ class TimerFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
+
+
